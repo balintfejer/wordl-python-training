8fb4a0fe580fbd29c1542331a21fd287a443febd
balintfejer <balint.fejer@gmail.com>
2024-02-26 12:35:13 +0100

    enabled session, random word generation, checking entered word TODO validate input len = 5; add JS to count games and reload dynamically

--- program/app.py
+++ program/app.py
@@ -9,82 +9,79 @@ import flask as fl
 ################################################################
 
 app = fl.Flask(__name__)
+app.config['SECRET_KEY'] = 'big!secret'
 
 @app.route('/')
 def welcome():
     return fl.render_template("index.html")
 
-@app.route('/play')
-def playy():
-    return fl.render_template("play.html")
-
-# def play(cond=None):
-
-#     def word_list():
-#         with open("words.txt", "r") as words:
-#             full_word_list = [line.strip() for line in words]
-#         return full_word_list
+@app.route('/start')
+def start():
+    def word_list():
+        with open("words.txt", "r") as words:
+            full_word_list = [line.strip() for line in words]
+        return full_word_list
+    goal = r.choice(word_list())
+    fl.session['goal'] = goal
     
-#     def check_guess(guess, goal):
+    # playcount = 1
+    # wincond = "XXXXX"
 
-#         this_guess_char_count = 0
-#         output = ""
+    print(goal) ### TEST
 
-#         for i in range(len(guess)):
-
-#             if guess[i] in goal:
-
-#                 if guess[i] == goal[i]:
-
-#                     this_guess_char_count += 1
-#                     output += "X"
-
-#                 elif guess[i] != goal[i]:
-
-#                     this_guess_char_count += 1
-
-#                     if guess.count(guess[i]) > goal.count(guess[i]) and guess[i] == goal[-1]:
-#                         output += "_"
-
-#                     elif guess.count(guess[i]) == goal.count(guess[i]) or this_guess_char_count == 1:
-#                         output += "O"
-
-#                     elif this_guess_char_count > 1:
-#                         output += "_"
-
-#             else: ### guess[i] not in goal
-#                 output += "_"
-
-#         return output
-
-#     # print(check_guess("carat", "train")) ### TEST
-#     # print(check_guess("taunt", "train")) ### TEST
-#     # print(check_guess("solos", "boobs")) ### TEST
-#     # print(check_guess("aabcd", "qqaqq")) ### TEST
-
-#     goal = r.choice(word_list())
-#     print(goal) ### TEST
-
-#     playcount = 1
-#     wincond = "XXXXX"
-#     won = "You won!"
-#     lost = f"You lost!\nThe word was: {goal}"
+    # won = "You won!"
+    # lost = f"You lost!\nThe word was: {goal}"
     
-#     while playcount <= 6:
+    # while playcount <= 6:
 
-#         playcount += 1
-#         guess = input("Pls guess a 5-letter word:").lower()
+    #     playcount += 1
+    #     guess = input("Pls guess a 5-letter word:").lower()
         
-#         if guess not in word_list():
-#             print("That's not a real word!")
-#         else:
-#             guess = check_guess(guess, goal)
-#             print(guess)
-#             if guess == wincond:
-#                 break
+    #     if guess not in word_list():
+    #         print("That's not a real word!")
+    #     else:
+    #         guess = check_guess(guess, goal)
+    #         print(guess)
+    #         if guess == wincond:
+    #             break
 
-#     if guess == wincond:
-#         return fl.render_template("index.html", cond=won)
-#     else:
-#         return fl.render_template("index.html", cond=lost)
-    
+    # if guess == wincond:
+    # else:
+
+    return fl.render_template("start.html")
+
+@app.route('/play', methods=["POST"])
+def play(output=None, guess=None):
+
+    guess = fl.request.form['guess'].lower()
+    goal = fl.session.get('goal', None)
+
+    this_guess_char_count = 0
+    output = ""
+
+    for i in range(len(guess)):
+
+        if guess[i] in goal:
+
+            if guess[i] == goal[i]:
+
+                this_guess_char_count += 1
+                output += "X"
+
+            elif guess[i] != goal[i]:
+
+                this_guess_char_count += 1
+
+                if guess.count(guess[i]) > goal.count(guess[i]) and guess[i] == goal[-1]:
+                    output += "_"
+
+                elif guess.count(guess[i]) == goal.count(guess[i]) or this_guess_char_count == 1:
+                    output += "O"
+
+                elif this_guess_char_count > 1:
+                    output += "_"
+
+        else: ### guess[i] not in goal
+            output += "_"
+
+    return fl.render_template("play.html", output=output, guess=guess)
